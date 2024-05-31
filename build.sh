294532d02ec5281f42a081ce4f61d2c2f5f0cc8f
David Zbarsky <david.zbarsky@airtable.com>
2024-05-30 21:23:55 -0400

    grafana 11

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -eux
 
 ls /home/linuxbrew/.linuxbrew/bin
 /home/linuxbrew/.linuxbrew/bin/brew fetch --verbose --os sonoma --arch arm grafana 
-tar -xf /home/runner/.cache/Homebrew/downloads/65e4d7e58696b3362cb45039f825177ca2d2d5918d62d76d9fd016a10a8b9540--grafana--10.3.1.arm64_sonoma.bottle.tar.gz
+tar -xf /home/runner/.cache/Homebrew/downloads/c02e4cf0855eb9a45d1b15dd1775e52847e7467a5da0f96ae4a3a4275a201493--grafana--11.0.0.arm64_sonoma.bottle.tar.gz
 
-mkdir -p grafana-v10.3.1/bin
-mv grafana/10.3.1/bin/grafana grafana-v10.3.1/bin/
-mv grafana/10.3.1/share/grafana/{public,conf} grafana-v10.3.1/
-tar -czf grafana_10.3.1_darwin_arm64.tar.gz grafana-v10.3.1
+mkdir -p grafana-v11.0.0/bin
+mv grafana/11.0.0/bin/grafana grafana-v11.0.0/bin/
+mv grafana/11.0.0/share/grafana/{public,conf} grafana-v11.0.0/
+tar -czf grafana_11.0.0_darwin_arm64.tar.gz grafana-v11.0.0
